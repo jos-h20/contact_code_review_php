ed66c2af1060f6f43cec814d6d77b27ce872b082
Josh Overly <joshoverly@yahoo.com>
2016-02-12 08:50:29 -0800

    add twig html and app router logic

--- app/app.php
+++ app/app.php
@@ -15,7 +15,21 @@
         'twig.path' => __DIR__.'/../views'
     ));
 
-    
+    $app->get("/", function() use ($app) {
+        return $app['twig']->render('contact.html.twig', array('contacts' => Contact::getAll()));
+    });
+
+    $app->post("/contacts", function() use ($app) {
+        $contact = new Contact($_POST['name'], $_POST['phone'], $_POST['street'], $_POST['city'], $_POST['state'], $_POST['zip']));
+        $contact->save();
+
+        return $app['twig']->render('create_contact.html.twig', array('newcontact' => $contact));
+    });
+
+    $app->post("/delete_contacts", function() use($app) {
+        Contact::deleteAll();
+        return $app['twig']->render('delete_contacts.html.twig');
+    });
 
     return $app;
 
